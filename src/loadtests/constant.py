ca96fa5f585f6f22381b7c2341355fa81e1305b7
MartinNeum <martin.neumann@capgemini.com>
2021-08-12 17:02:40 +0200

    OPS-2650 Added checkup for existing environment variables and alternative values

--- src/loadtests/constant.py
+++ src/loadtests/constant.py
@@ -5,15 +5,37 @@ from locust import between
 class constant():
     wait_time = between(5, 15) # Provides a random number which will be used as waiting time for the users
     MATRIX_MESSENGER = os.environ.get("MMHOST")
-    timeToWaitShort = int(os.environ.get("TIMESHORT"))
-    timeToWaitLong = int(os.environ.get("TIMELONG"))
+    
+    if os.environ.get("TIMESHORT") is not None:
+        timeToWaitShort = int(os.environ.get("TIMESHORT"))
+    else:
+        print(f"\n#### ALTERNATIV 1 ####\n")
+        timeToWaitShort = 5
+    
+    if os.environ.get("TIMELONG") is not None:
+        timeToWaitShort = int(os.environ.get("TIMELONG"))
+    else:
+        print(f"\n#### ALTERNATIV 2 ####\n")
+        timeToWaitShort = 10
+
     returncodeNormal = 200 # Returncode if a Request is successful
     returncodeRedirect = 302 # Returncode if a Request is successful and the user was redirected on an other website
     returncodeCreated = 201 # Returncode if a Request to create something was successful
     bBBKey = os.environ.get("BBBKEY")
     bBBHost = os.environ.get("BBBHOST")
-    numberRooms = int(os.environ.get("BBBNUMBERROOMS"))
-    numberUsers = int(os.environ.get("BBBNUMBERUSERS"))
+    
+    if os.environ.get("BBBNUMBERROOMS") is not None:
+        timeToWaitShort = int(os.environ.get("BBBNUMBERROOMS"))
+    else:
+        print(f"\n#### ALTERNATIV 3 ####\n")
+        timeToWaitShort = 3
+    
+    if os.environ.get("BBBNUMBERUSERS") is not None:
+        timeToWaitShort = int(os.environ.get("BBBNUMBERUSERS"))
+    else:
+        print(f"\n#### ALTERNATIV 4 ####\n")
+        timeToWaitShort = 6
+
     urlBetterMarks = os.environ.get("URLBETTERMARKS") # required for downloading bettermarks-tools
 
     # Get the user credentials for locust HttpUser. Either the file-path or the constants will be used, other wise the programm will exit.
