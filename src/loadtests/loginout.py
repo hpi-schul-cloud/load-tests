f23eff6973cb0e260346788e268085bf6595e7b0
fblauza <fblauza@CE30870>
2021-08-18 08:31:20 +0200

    Test changes

--- src/loadtests/loginout.py
+++ src/loadtests/loginout.py
@@ -1,6 +1,7 @@
 import base64
 import os
 import json
+import codecs
 from loadtests import constant
 
 from loadtests.functions import *
@@ -32,7 +33,8 @@ def login(self):
                 response_header = login_post_response.headers
                 #Extracting BearerToken from Responses Header
                 self.bearer_token = (response_header["set-cookie"]).split(";")[0].replace("jwt=", "")
-                decoded_token = base64.b64decode(self.bearer_token[0:461])
+                decoded_token = codecs.decode(self.bearer_token[0:461], 'base64')
+                #decoded_token = base64.b64decode(self.bearer_token[0:461])
                 decoded_token_json = json.loads(decoded_token.decode('utf_8').removeprefix('{"alg":"HS256","typ":"access"}'))
                 self.user_id = decoded_token_json["userId"]
                 self.school_id = decoded_token_json["schoolId"]
