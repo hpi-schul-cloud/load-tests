dd663b23d068436be8ae0d8877653e7a8d7a220e
fblauza <fblauza@CE30870>
2021-08-23 15:09:58 +0200

    OPS-2649-Exception-during-loginout-process-in-loadtest

--- src/loadtests/loginout.py
+++ src/loadtests/loginout.py
@@ -33,7 +33,7 @@ def login(self):
                 response_header = login_post_response.headers
                 #Extracting BearerToken from Responses Header
                 self.bearer_token = (response_header["set-cookie"]).split(";")[0].replace("jwt=", "")
-                decoded_token = base64.b64decode(self.bearer_token[0:461]).decode('utf-8') #base64.decodestring(str.encode(self.bearer_token[0:461]))
+                decoded_token = base64.decodestring(str.encode(self.bearer_token[0:461])).decode('utf-8') #base64.b64decode(self.bearer_token[0:461]).decode('utf-8') #
                 decoded_token_json = json.loads(str(decoded_token).removeprefix('{"alg":"HS256","typ":"access"}'))
                 self.user_id = decoded_token_json["userId"]
                 self.school_id = decoded_token_json["schoolId"]
