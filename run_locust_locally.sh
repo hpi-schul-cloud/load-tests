6fd435d1d555fe960bba5a6ec05b9a4a4d9bfce8
Andre Blome <andre-david.blome@capgemini.com>
2022-02-23 10:09:16 +0100

    test.py prettier, script for running func test locally, secrets can be anywhere now

--- run_locust_locally.sh
+++ run_locust_locally.sh
@@ -8,12 +8,11 @@ export ADMIN_WEIGHT=1
 export TEACHER_WEIGHT=1
 export PUPIL_WEIGHT=1
 export ANONYMOUS_WEIGHT=1
-source ../loadtest_secrets.env
 
 HOST=https://agmonlog-1.hpi-schul-cloud.dev
 USER=4
 TIME=10s
 LOGLEVEL=INFO
 
-pip install locust -q
+pip install -r requirements.txt -q
 PYTHONPATH=src locust -f src/loadtests/loadtests/locustfile.py -H $HOST -u $USER -t $TIME --headless -L $LOGLEVEL --only-summary
