6fc78ff5d20ec22d1ead0fbbd8fcd8ab972a824a
Dimitrij Pinneker <dimitrij.pinneker@capgemini.com>
2022-03-25 14:04:16 +0100

    OPS-3094 Fixed some smaller issues

--- run_functional_locally.sh
+++ run_functional_locally.sh
@@ -2,4 +2,4 @@
 export FUNCTIONAL_TEST=1
 
 pip install -r requirements.txt -q
-PYTHONPATH=src python src/loadtests/functionaltests/test.py
+PYTHONPATH=src python3 src/loadtests/functionaltests/main.py
